4e6dca1b8556ef505023ed21ac19f2b3ea22d369
Sameer Asif <sameer.asif.mughal@gmail.com>
2024-07-07 11:02:33 -0700

    Workout Section

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SectionWrapper from './SectionWrapper'
+import { SCHEMES, WORKOUTS } from '../utils/excercises'
+
+    function Header(props){
+        const {index, title, description} = props
+        return (
+            <div className='flex flex-col gap-4'>
+                <div className='flex items-center justify-center gap-4'>
+                <p className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-rose-400'>{index}</p>  
+                <h4 className='text-xl sm:text-xl md:text-2xl lg:text-3xl'>{title}</h4>
+                </div>
+                <p className='mx-auto text-base sm:text-base md:text-lg lg:text-xl'>{description}</p>
+            </div>
+        )
+    }
 
 export default function Generator() {
-  return (
-    <SectionWrapper header={'generate your custom workout'} title={['It\'s', 'time', 'to', 'get', 'Big']}>
-           
+    const [showModal, setShowModal]=useState(false);
+    const [split, setSplit]=useState('individual')
+    const [muscles, setMuscles] = useState([])
+    const [goals, setGoals] = useState('strength_power')
 
-    </SectionWrapper>
+    function dropdown(){
+       setShowModal(!showModal);
+    }
+
+  return (
+    <div>
+    <SectionWrapper header={'Generate your custom workout'} title={['It\'s', 'time', 'to', 'get', 'HUGE']} />
+    <Header index={'01'} title={'Pick your poison'} description={"Select the workout you wish to enjoy"} />
+    <div className='flex gap-10 justify-center mt-5 flex-wrap'>
+    {Object.keys(WORKOUTS).map((type, typeIndex )=>{
+        return(
+            <button key={typeIndex} onClick={()=>{
+                setSplit(type)
+            }} className='px-5 py-3 rounded-md mt-5 boxShadow border-2 border-rose-700 capitalize'>
+               {type.replaceAll('_'," ")}
+                </button>
+        )
+    })}
+    </div>
+    <div className='mt-20'>
+    <Header index={'02'} title={'Lock on targets'} description={"Select the muscles to annihilate"}/>
+             <div className='border-2 border-rose-700 p-2 rounded-lg mx-20 sm:mx-20 md:mx-70 lg:mx-[35%] flex justify-center items-center relative mt-5 flex flex-col'>
+                    <p>Select muscle groups</p>
+                    <i className="fa-solid fa-caret-down absolute right-3" onClick={dropdown}></i>
+                    {showModal &&  (
+                    <div>Modal </div>
+                 )}
+                 </div>
+                 
+         </div>
+         <div className='mt-20'>
+    <Header index={'03'} title={'Become Juggernaut'} description={"Select your ultimate objective."}/>
+    <div className='flex gap-10 justify-center mt-5 flex-wrap'>
+            {Object.keys(SCHEMES).map((scheme, schemeIndex )=>{
+        return(
+            <button key={schemeIndex} className='px-5 py-3 rounded-md mt-5 boxShadow border-2 border-rose-700 capitalize'>
+               {scheme.replaceAll('_'," ")}
+                </button>
+              )
+             })}
+                </div>
+                 
+         </div>
+    </div>
+    
   )
 }
--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
 export default function SectionWrapper(props) {
-    const  {children} = props
+    const  {header, title} = props
   return (
-    <div>{children}</div>
+    <section className='min-h-3.5 flex flex-col gap-10'>
+        <div className='bg-slate-950 py-10 flex flex-col gap-4 justify-center items-center'>
+            <p className='text-lg md:text-lg sm:text-sm'>{header}</p> 
+            <h2 className='text-xl sm:text-2xl md:text-3xl lg:text-5xl'>{title[0]} {title[1]} {title[2]} {title[3]} <span className='text-rose-700 font-extrabold'>{title[4]}</span></h2>
+        </div>
+        <div className='max-w-[800px] w-full flex flex-col mx-auto gap-10'></div>
+    </section>
   )
 }
