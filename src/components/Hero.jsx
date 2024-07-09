96919e7b5e53f742b3768391381d5641bdd52078
Sameer Asif <sameer.asif.mughal@gmail.com>
2024-07-09 09:18:23 -0700

    Fixed Description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,9 @@ export default function Hero() {
         <p className='text-lg text-center'>GRIND FOR THE</p>
         <h1 className='font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl mt-0 text-rose-700 textShadow'>GAINZ</h1>
         </div>
-        <p className='mt-10 font-light w-8/12 text-center sm:text-sm md:text-lg lg:text-lg'>Getting fit isn't just about exercise. It's about <span className='text-rose-700 font-extrabold'> feeling great </span> , <span className='text-rose-700 font-extrabold'>having fun</span>,
-             and <span className='text-rose-700 font-extrabold'>challenging yourself</span>. At GainZ, we foster a supportive community that 
-             motivates and inspires you along the way. We offer a variety of programs
-             to keep your workouts exciting. Remember, fitness is a journey, not a destination. Let us help you make it enjoyable and rewarding! </p>
+        <p className='mt-10 font-light w-8/12 text-center sm:text-sm md:text-lg lg:text-lg'>
+        Ditch the Routine, Unleash the Results: Personalized Workouts at Your Fingertips.
+        <span className='text-rose-500'>  Struggling to find the perfect workout plan?</span> Dreading another gym session full of the same old exercises? We get it. That's why we created GainZ, your one-stop shop for personalized workouts that keep you <span className='text-rose-500'>engaged</span> and<span className='text-rose-500'>on track.</span>  </p>
          <MainButton func={()=>{
           window.location.href='#generate'
          }} text={'Apply'} id={'apply'}/>
